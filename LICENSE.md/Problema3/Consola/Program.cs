133c4d0651afff66671e99916f5d2a15fb4d16aa
maxauz <apaza.maximiliano@gmail.com>
2017-04-24 07:21:00 -0500

    Add files via upload

---
+++ LICENSE.md/Problema3/Consola/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Problema3, Ingrese el monto:");
+            string numeros = Console.ReadLine();
+            Console.WriteLine("El resultado es:");
+            MoneyParts.build(numeros);
+            Console.ReadLine();
+        }
+    }
+}
